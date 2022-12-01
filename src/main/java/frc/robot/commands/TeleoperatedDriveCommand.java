e53988d6385ee7ad5a887e4f75a98f8f793a4272
CodyW <CodyW2125@outlook.com>
2022-12-01 16:57:49 -0600

    Fixed K_Drives, added N_Drives, Complete?

--- src/main/java/frc/robot/commands/TeleoperatedDriveCommand.java
+++ src/main/java/frc/robot/commands/TeleoperatedDriveCommand.java
@@ -3,10 +3,9 @@ package frc.robot.commands;
 
 //Local
 import frc.robot.subsystems.SwerveSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
 //Libraries
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import java.util.Objects;
 
 import com.ctre.phoenix.sensors.Pigeon2;
@@ -47,13 +46,37 @@ public class TeleoperatedDriveCommand extends CommandBase
   @Override
   public void execute() 
   {
-    //Get Drive and Rotational Motors
-    MotorControllerGroup K_Drive = Parent_Subsystem.getKDrive();
-    MotorControllerGroup K_Rotational = Parent_Subsystem.getKRotate();
+    //Motor Assignment
+    WPI_TalonSRX K_Drive_Motor = Parent_Subsystem.getKDrive()[(JoystickR_X > 0)? (1): ((JoystickR_X < 0)? (0): (0))];
+    WPI_TalonSRX K_Rotational_Motor = Parent_Subsystem.getKRotate()[(JoystickR_X > 0)? (1): ((JoystickR_X < 0)? (0): (0))];
+    WPI_TalonSRX[] N_Drive_Motors = new WPI_TalonSRX[3];
+    WPI_TalonSRX[] N_Rotational_Motors = new WPI_TalonSRX[3];
+    //Retrieval of N[D]
+    for(int i = 0, j = 0; i < Parent_Subsystem.getNDrives().length; i++)
+    {
+      for(WPI_TalonSRX N_Drive_Motor: Parent_Subsystem.getNDrives()[i])
+      {
+        if(!(Objects.equals(N_Drive_Motor,K_Drive_Motor))){N_Drive_Motors[j] = N_Drive_Motor;j++;}
+      }
+    }
+    //Retrieval of N[R]
+    for(int i = 0, j = 0; i < Parent_Subsystem.getNRotates().length; i++)
+    {
+      for(WPI_TalonSRX N_Rotate_Motor: Parent_Subsystem.getNRotates()[i])
+      {
+        if(!(Objects.equals(N_Rotate_Motor,K_Rotational_Motor))){N_Rotational_Motors[j] = N_Rotate_Motor;j++;}
+      }
+    }
+    //Execution
+    K_Drive_Motor.set(Math.pow(JoystickL_Y,2));
+    K_Rotational_Motor.set(Math.atan((180/(JoystickR_X * 100))));
+    for(WPI_TalonSRX N_Drive:N_Drive_Motors){N_Drive.set(Math.pow(JoystickL_Y,2));}
+    for(WPI_TalonSRX N_Rotates:N_Rotational_Motors){N_Rotates.set(Math.atan(JoystickL_Y/JoystickL_X));}
   }
   //End Command
   @Override
   public void end(boolean interrupted) {if(Objects.equals(interrupted,true)){Command_Complete = true;}}
+
   //Check Command Complete
   @Override
   public boolean isFinished() {return Command_Complete;}
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -4,8 +4,8 @@ package frc.robot.subsystems;
 //Libraries
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.sensors.Pigeon2;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.Objects;
 
 //SwerveSubSystem
 public class SwerveSubsystem extends SubsystemBase
@@ -21,28 +21,22 @@ public class SwerveSubsystem extends SubsystemBase
   private WPI_TalonSRX R_FR;
   private WPI_TalonSRX R_BL;
   private WPI_TalonSRX R_BR;
-  //Large Groups
-  private MotorControllerGroup Rotational;
-  private MotorControllerGroup Drive;
-  //Rotational Groups
-  private MotorControllerGroup F_Rotational;
-  private MotorControllerGroup L_Rotational;
-  private MotorControllerGroup B_Rotational;
-  private MotorControllerGroup R_Rotational;
-  private MotorControllerGroup K_Rotational;
-  //Drive Groups
-  private MotorControllerGroup F_Drive;
-  private MotorControllerGroup L_Drive;
-  private MotorControllerGroup B_Drive;
-  private MotorControllerGroup R_Drive;
-  private MotorControllerGroup K_Drive;
+  //Large List
+  private WPI_TalonSRX[] Rotational;
+  private WPI_TalonSRX[] Drive;
+  //S-Rotational List
+  private WPI_TalonSRX[] K_Rotational;
+  private WPI_TalonSRX[] [] N_Rotationals;
+  //S-Drive List
+  private WPI_TalonSRX[] K_Drive;
+  private WPI_TalonSRX[] [] N_Drives;
   //Gyroscope
   final Pigeon2 M_Gyro;
   //Rotational Face
   private int R_Face;
   //Group Lists
-  private MotorControllerGroup[] Rotational_Groups;
-  private MotorControllerGroup[] Drive_Groups;
+  private WPI_TalonSRX[] [] Rotational_Groups;
+  private WPI_TalonSRX[] [] Drive_Groups;
   //Compass Heading
   private static double Compass_Heading;
   //Constructors
@@ -60,25 +54,15 @@ public class SwerveSubsystem extends SubsystemBase
     R_BL = new WPI_TalonSRX(3);
     R_BR = new WPI_TalonSRX(7);
     //Large Groups
-    Rotational = new MotorControllerGroup(R_FL,R_FR,R_BL,R_BR);
-    Drive = new MotorControllerGroup(D_FL,D_FR,D_BL,D_BR);
-    //Rotational Groups
-    F_Rotational = new MotorControllerGroup(R_FL,R_FR);
-    L_Rotational = new MotorControllerGroup(R_FL,R_BL);
-    B_Rotational = new MotorControllerGroup(R_BL,R_BR);
-    R_Rotational = new MotorControllerGroup(R_FR,R_BR);
-    //Drive Groups
-    F_Drive = new MotorControllerGroup(D_FL, D_FR);
-    L_Drive = new MotorControllerGroup(D_FL, D_BL);
-    B_Drive = new MotorControllerGroup(D_BL,D_BR);
-    R_Drive = new MotorControllerGroup(D_BR, D_FR);
+    Rotational = new WPI_TalonSRX[]{R_FL,R_FR,R_BL,R_BR};
+    Drive = new WPI_TalonSRX[]{D_FL,D_FR,D_BL,D_BR};
     //Additional
     M_Gyro = Gyro;
     //Rotational Face
     R_Face = 1;
     //Group Lists
-    Rotational_Groups = new MotorControllerGroup[] {F_Rotational,L_Rotational,B_Rotational,R_Rotational};
-    Drive_Groups = new MotorControllerGroup[] {F_Drive,L_Drive,B_Drive,R_Drive};
+    Rotational_Groups = new WPI_TalonSRX[] [] {{R_FL,R_FR},{R_FL,R_BL},{R_BL,R_BR},{R_FR,R_BR}};
+    Drive_Groups = new WPI_TalonSRX[] [] {{D_FL, D_FR},{D_FL, D_BL},{D_BL,D_BR},{D_BR, D_FR}};
     //Compass Heading
     Compass_Heading = M_Gyro.getCompassHeading();
 
@@ -94,30 +78,38 @@ public class SwerveSubsystem extends SubsystemBase
   {
     //Update Heading and Rotational Wheels
     RotationalWheels(Compass_Heading = M_Gyro.getCompassHeading());
+    
   }
 
   //Simulation Periodic
   @Override
   public void simulationPeriodic() {}
 
-  //Convert K-Motors
+  //Convert K-Motors, R-Motors
   public void RotationalWheels(double Heading)
   {
-    K_Drive = Drive_Groups[(((int)Math.round(Heading/90) + R_Face) > 4)? (0): ((int)Math.round(Heading/90))];
-    K_Rotational = Rotational_Groups[(((int)Math.round(Heading/90) + R_Face) > 4)? (0): ((int)Math.round(Heading/90))];
+    int Index = (((int)Math.round(Heading/90) + R_Face) > Rotational_Groups.length)? (0): ((int)Math.round(Heading/90));
+    K_Drive = Drive_Groups[Index];
+    K_Rotational = Rotational_Groups[Index];
+    for(int i = 0, j = 0; i < Drive_Groups.length; i++)
+      if(!(Objects.equals(i,Index))){N_Rotationals[j] = Rotational_Groups[i];N_Drives[j] = Drive_Groups[i];j++;}
   }
 
   //ACESSORS
   //Return Current Rotational Face
   public int getRotationalFace() {return R_Face;}
   //Return K[D] MotorControllerGroup
-  public MotorControllerGroup getKDrive() {return K_Drive;}
+  public WPI_TalonSRX[] getKDrive() {return K_Drive;}
   //Return K[R] MotorControllerGroup
-  public MotorControllerGroup getKRotate() {return K_Rotational;}
+  public WPI_TalonSRX[] getKRotate() {return K_Rotational;}
   //Return Rotaional Group
-  public MotorControllerGroup getRotationalGroup() {return Rotational;}
+  public WPI_TalonSRX[] getRotationalGroup() {return Rotational;}
   //Return Drive Group
-  public MotorControllerGroup getDriveGroup() {return Drive;}
+  public WPI_TalonSRX[] getDriveGroup() {return Drive;}
+  //Return N[D]
+  public WPI_TalonSRX[] [] getNRotates() {return N_Rotationals;}
+  //Return N[R]
+  public WPI_TalonSRX[] [] getNDrives() {return N_Drives;}
   //Return Current Compass Heading
   public double getCurrentHeading() {return Compass_Heading;}
 }
